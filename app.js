eaf8369419fc2edd242574ca9971a047d51fd2e3
nikita stepanov <nist1701@gmail.com>
2023-02-05 01:11:12 +0300

    hotfix/profile name centering

--- app.js
+++ app.js
@@ -9,21 +9,12 @@ const inputBio = document.querySelector('#input-bio');
 // name and bio values
 const currentName = document.querySelector('#profile-name');
 const currentBio = document.querySelector('#profile-bio');
-// save button
-const saveButton = document.querySelector('#save-button');
-
 
 // open state
 profileEditButton.addEventListener('click', () => {
     popup.classList.add('popup_opened');
     inputName.value = currentName.innerText;
     inputBio.value = currentBio.innerText;
-
-    saveButton.addEventListener('click', () => {
-        currentName.innerText = inputName.value;
-        currentBio.innerText = inputBio.value;
-        closePopup();
-    })
 })
 // closed state
 popupCloseButton.addEventListener('click', closePopup);
@@ -32,14 +23,15 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-
-// prevent refresh
-form.addEventListener('submit', (e) => {
+// prevent refresh and overwrite name + bio
+form.addEventListener('submit', e => {
     e.preventDefault();
+    currentName.innerText = inputName.value;
+    currentBio.innerText = inputBio.value;
+    closePopup();
 })
 
-
-
+// change state of like buttons
 const likeButtons = document.querySelectorAll('#like-button');
 likeButtons.forEach(button => button.addEventListener('click', () => {
     button.classList.toggle('element__like_active')
