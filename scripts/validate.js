353009931f0a4b8b03869c9452132cd347b858a0
nikita stepanov <nist1701@gmail.com>
2023-02-14 17:11:05 +0300

    hotfix/form reset + button state fix

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,19 +1,17 @@
-
-
-  const showInputError = (form, input, errorMessage, classes) => {
+const showInputError = (form, input, errorMessage, classes) => {
     const errorElement = form.querySelector(`#${input.id}-error`);
     input.classList.add(classes.inputErrorClass);
     errorElement.textContent = errorMessage;
     errorElement.classList.add(classes.errorClass);
-  };
+};
   
 
-  const hideInputError = (form, input, classes) => {
+const hideInputError = (form, input, classes) => {
     const errorElement = form.querySelector(`#${input.id}-error`);
     input.classList.remove(classes.inputErrorClass);
     errorElement.classList.remove(classes.errorClass);
     errorElement.textContent = '';
-  };
+};
   
 const toggleButtonState = (button, validState) => {
   validState 
@@ -21,9 +19,9 @@ const toggleButtonState = (button, validState) => {
   : button.setAttribute('disabled', true)
 }
 
-const checkInputValidity = input => input.validity.valid
+const checkInputValidity = input => input.validity.valid;
 
-  const checkValidity = (form, input, formButton, classes) => {
+const checkValidity = (form, input, formButton, classes) => {
     const isValidInput = checkInputValidity(input)
     const allFormInputs = form.querySelectorAll(classes.inputSelector)
     const allFormInputsList = Array.from(allFormInputs)
@@ -36,9 +34,9 @@ const checkInputValidity = input => input.validity.valid
     } else {
         showInputError(form, input, input.validationMessage, classes)
     }
-  }
+}
 
-  const setEventListeners = (form, classes) => {
+const setEventListeners = (form, classes) => {
     const inputs = Array.from(form.querySelectorAll(classes.inputSelector))
     const formButton = form.querySelector(classes.submitButtonSelector)
 
@@ -53,21 +51,21 @@ const checkInputValidity = input => input.validity.valid
     inputs.forEach(input => {
         input.addEventListener('input', () => checkValidity(form, input, formButton, classes))
     })
-  }
+}
 
-  const enableValidation = (...selectors) => {
+const enableValidation = (...selectors) => {
     const forms = Array.from(document.forms)
     const classes = selectors[0]
     forms.forEach(form => {
         setEventListeners(form, classes)
     })
 
-  }
+}
 
-  enableValidation({
+enableValidation({
     formSelector: '.popup__form',
     inputSelector: '.form__input',
     submitButtonSelector: '.form__submit-button',
     inputErrorClass: 'form__input_type_error',
     errorClass: 'form__input-error_active'
-  }); 
+}); 
